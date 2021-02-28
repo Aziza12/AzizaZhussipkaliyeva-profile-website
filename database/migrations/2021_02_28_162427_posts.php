d735f1e69073b6d43be4939f4aa1966388f46b97
Aziza Devops <190103128@stu.sdu.edu.kz>
2021-02-28 22:29:01 +0600

    3,4 task create and post

--- database/migrations/2021_02_28_162427_posts.php
+++ database/migrations/2021_02_28_162427_posts.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Post extends Migration
+class Posts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class Post extends Migration
      */
     public function up()
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('Title');
-            $table->string('Body');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class Post extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('posts');
     }
 }
